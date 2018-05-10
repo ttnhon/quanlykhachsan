a904eb43d776ba72624f6488d78b9bc7e0696072
NVPIT\NVP <nvpit97@gmail.com>
2018-05-10 22:35:48 +0700

    thêm data cho bảng "DichVu" + "LoaiPhong"

--- insert_data.sql
+++ insert_data.sql
@@ -1,4 +1,7 @@
 ﻿--Insert <PhanQuyen>
+use quanlykhachsan
+go
+
 insert into PhanQuyen(LoaiNguoiDung, MucDo)
 values	(N'Giám đốc', 1),
 		(N'Quản lý', 2),
@@ -25,8 +28,21 @@ insert into TrangThaiPhong(TrangThai)
 values (N'Khách trong phòng'), (N'Khách ra ngoài'), (N'Đang dọn dẹp'), (N'Đang sửa chữa')
 go
 --Insert <LoaiPhong>
---Insert <DichVu>
+insert into LoaiPhong(MaLoai, TenLoai, MoTa, DonGia, SoNguoiToiDa)
+values  ('1',N'thường',N'Loại phòng phổ thông',500000,3),
+		('2',N'chuẩn',N'Loại phòng trung lưu, tiện nghi khá',1000000,2),
+		('3',N'tầm nhìn đẹp',N'Tầm nhìn đẹp và thoáng đãng',1200000,2),
+		('4',N'cặp đôi',N'Dành cho cặp đôi',700000,2),
+		('5',N'vip',N'Chuẩn cao cấp, tiện nghi sang trọng',1500000,2)
 
+--Insert <DichVu>
+insert into DichVu(TenDV, DonGia, GhiChu)
+values	(N'Thức ăn',100000,N'Có nhiều món để chọn lựa'),
+		(N'Điểm tâm sáng',100000,N'Có nhiều món để chọn lựa'),
+		(N'Thức ăn nhẹ',50000,N'Có nhiều món để chọn lựa'),
+		(N'Thức uống',50000,N'Có nhiều loại thức uống để chọn lựa'),
+		(N'Massage',200000,N'Không'),
+		(N'BCS',25000,N'Ưu tiên phòng cặp đôi')
 --Insert <DatPhong>
 --Insert <ChiTietDichVu>
 --Insert <ThuePhong>
