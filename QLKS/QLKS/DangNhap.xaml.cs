a21f0a51414cf7c08f2c64143c7a11a61e0a5840
DESKTOP-VIIL54U\phiph <phiphuc27@gmail.com>
2018-05-11 22:23:55 +0700

    Thêm giao diện đăng nhập

--- QLKS/QLKS/DangNhap.xaml.cs
+++ QLKS/QLKS/DangNhap.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data;
 
 namespace QLKS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DangNhap.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DangNhap : Window
     {
-        public MainWindow()
+        public DangNhap()
         {
             InitializeComponent();
-            DangNhap dangnhap = new DangNhap();
-            dangnhap.Show();
-            //DataTable table = LoaiPhongDAO.LoadAll();
-            //dataGrid.ItemsSource = table.DefaultView;
         }
     }
 }
