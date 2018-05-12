f1b92720cabac2e8b8e7395a217885398f878d6a
REFICUL\Reficul <ttnhon0404@gmail.com>
2018-05-12 14:24:20 +0700

    Chỉnh sửa dữ liệu

--- insert_data.sql
+++ insert_data.sql
@@ -18,10 +18,10 @@ values	(N'Trương Thành Nhơn', N'Giám đốc', 'ttnhon', '1'),
 go
 --Insert <KhachHang>
 insert into KhachHang(TenKhach,SoCMND,DiaChi,DienThoai,LoaiKhach)
-values  (N'Trần Văn Tài','0123456789',N'123 Nguyễn Tri Phương','0906758412','1'),
-		(N'Nguyễn Thị Mơ','0365874165',N'412 Nguyễn Văn Cừ','0122486531','1'),
-		(N'John Wick','0356916327',N'2 Tôn Đức Thắng','0909555142','2'),
-		(N'Huỳnh Nguyễn Hoài Lâm','0856975423',N'54 Hòa Bình','0188645314','1')
+values  (N'Trần Văn Tài','0123456789',N'Hà Nội','0906758412','1'),
+		(N'Nguyễn Thị Mơ','0365874165',N'Tp HCM','0122486531','1'),
+		(N'John Wick','0356916327',N'Hải Phòng','0909555142','2'),
+		(N'Huỳnh Nguyễn Hoài Lâm','0856975423',N'Vũng Tàu','0188645314','1')
 --Insert <Phong>
 insert into Phong(LoaiPhong,TinhTrang,TrangThai,GhiChu)
 values	('1','1','3',N'Không có'),
@@ -47,25 +47,25 @@ insert into TrangThaiPhong(TrangThai)
 values (N'Khách trong phòng'), (N'Khách ra ngoài'), (N'Đang dọn dẹp'), (N'Đang sửa chữa')
 go
 --Insert <LoaiPhong>
-insert into LoaiPhong(MaLoai, TenLoai, MoTa, DonGia, SoNguoiToiDa)
-values  ('STD A', N'Standard A', N'2 giường đơn / 1 giường đôi', 680000, 2),
-		('STD B', N'Standard B', N'3 giường đơn / 1 giường đôi + 1 giường đơn', 850000, 3),
-		('STD C', N'Standard C', N'4 giường đơn / 2 giường đôi', 910000, 4),
-		('SUP A', N'Superior A', N'Chất lượng cao hơn Standard, 2 giường đơn/1 giường đôi', 890000, 2),
-		('SUP B', N'Superior B', N'Chất lượng cao hơn Standard, 3 giường đơn / 1 giường đôi + 1 giường đơn', 910000, 3),
-		('SUP C', N'Superior C', N'Chất lượng cao hơn Standard, 2 giường đơn + 1 giường đôi', 1100000, 4),
-		('DLX', N'Deluxe',N'Chất lượng cao hơn Superior, 2 giường đơn / 1 giường đôi', 1100000, 2),
-		('SUT A', N'Suite A',N'Chất lượng cao hơn Deluxe, 1 phòng ngủ + 1 phòng khách', 1500000, 2),
-		('SUT B', N'Suite B',N'Chất lượng cao hơn Deluxe, 2 phòng ngủ + 1 phòng khách', 1700000, 4)
+insert into LoaiPhong(MaLoai, TenLoai, MoTa, DonGia, SoNguoiToiDa, ConSuDung)
+values  ('STD A', N'Standard A', N'2 giường đơn / 1 giường đôi', 680000, 2, 1),
+		('STD B', N'Standard B', N'3 giường đơn / 1 giường đôi + 1 giường đơn', 850000, 3, 1),
+		('STD C', N'Standard C', N'4 giường đơn / 2 giường đôi', 910000, 4, 1),
+		('SUP A', N'Superior A', N'Chất lượng cao hơn Standard, 2 giường đơn/1 giường đôi', 890000, 2, 1),
+		('SUP B', N'Superior B', N'Chất lượng cao hơn Standard, 3 giường đơn / 1 giường đôi + 1 giường đơn', 910000, 3, 1),
+		('SUP C', N'Superior C', N'Chất lượng cao hơn Standard, 2 giường đơn + 1 giường đôi', 1100000, 4, 1),
+		('DLX', N'Deluxe',N'Chất lượng cao hơn Superior, 2 giường đơn / 1 giường đôi', 1100000, 2, 1),
+		('SUT A', N'Suite A',N'Chất lượng cao hơn Deluxe, 1 phòng ngủ + 1 phòng khách', 1500000, 2, 1),
+		('SUT B', N'Suite B',N'Chất lượng cao hơn Deluxe, 2 phòng ngủ + 1 phòng khách', 1700000, 4, 1)
 
 --Insert <DichVu>
-insert into DichVu(TenDV, DonGia, GhiChu)
-values	(N'Thức ăn',100000,N'Có nhiều món để chọn lựa'),
-		(N'Điểm tâm sáng',100000,N'Có nhiều món để chọn lựa'),
-		(N'Thức ăn nhẹ',50000,N'Có nhiều món để chọn lựa'),
-		(N'Thức uống',50000,N'Có nhiều loại thức uống để chọn lựa'),
-		(N'Massage',200000,N'Không'),
-		(N'BCS',25000,N'Ưu tiên phòng cặp đôi')
+insert into DichVu(TenDV, DonGia, GhiChu, ConSuDung)
+values	(N'Thức ăn',100000,N'Có nhiều món để chọn lựa', 1),
+		(N'Điểm tâm sáng',100000,N'Có nhiều món để chọn lựa', 1),
+		(N'Thức ăn nhẹ',50000,N'Có nhiều món để chọn lựa', 1),
+		(N'Thức uống',50000,N'Có nhiều loại thức uống để chọn lựa', 1),
+		(N'Massage',200000,N'Không', 1),
+		(N'BCS',25000,N'Ưu tiên phòng cặp đôi', 1)
 --Insert <DatPhong>
 --Insert <ChiTietDichVu>
 --Insert <ThuePhong>
