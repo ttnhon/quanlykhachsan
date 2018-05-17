db09236bc06626fb1fe347b5f2753b7e82714a33
REFICUL\Reficul <ttnhon0404@gmail.com>
2018-05-17 21:29:02 +0700

    Thềm màn hình Thuê Phòng

--- QLKS/QLKS/ThuePhong.xaml.cs
+++ QLKS/QLKS/ThuePhong.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -22,6 +23,20 @@ namespace QLKS
         public ThuePhong()
         {
             InitializeComponent();
+
+            System.Windows.Controls.Button myBtn = new Button();
+            myBtn.Content = "Test button 7";
+            myBtn.Name = "btn7";
+            RoutedEventHandler a = new RoutedEventHandler(Button_Click);
+            myBtn.Click += a;
+            wrapPanel.Children.Add(myBtn);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            if(button != null)
+               MessageBox.Show(button.Name);
         }
     }
 }
